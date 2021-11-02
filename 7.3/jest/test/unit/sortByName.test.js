08ed5022c72c425c2950ad33187d2cdb76bfa87a
Oksana Melnikova <36515885+Melnioks@users.noreply.github.com>
2021-11-02 23:58:21 +0100

    Add files via upload

---
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -0,0 +1,17 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
