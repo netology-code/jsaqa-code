7fbe1dc04ac686446dbba9238f8599b8766124a1
Oksana M <36515885+Melnioks@users.noreply.github.com>
2021-11-16 16:15:32 +0100

    Add files via upload

---
+++ 7.6/cypress/support/commands.js
@@ -0,0 +1,32 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (login, password) => {
+  cy.contains("Log in").click();
+  cy.get("#mail").type(login);
+  cy.get("#pass").type(password);
+  cy.contains("Submit").click();
+});
