2df71537c941ca84a0f516df0119e48eca6b41a3
gluk2309 <gluk2309@mail.ru>
2024-02-14 13:28:44 +0300

    Added screenshots

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,12 +1,15 @@
 const { test, expect } = require("@playwright/test");
 const { email, password } = require("../user.js");
+const path = require("path");
 
 const faker = require("faker");
 
 const email2 = faker.internet.email();
 const password2 = faker.internet.password();
 
-test("Authorization is succsesfull", async ({ page }) => {
+const screenshotsDir = path.join(__dirname, "screenshots");
+
+test("Authorization is successful", async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder("Email").fill(email);
   await page.getByPlaceholder("Пароль").fill(password);
@@ -14,16 +17,17 @@ test("Authorization is succsesfull", async ({ page }) => {
 
   await expect(page).toHaveURL("https://netology.ru/profile/8169730");
   await page.waitForSelector("h2");
+  await page.screenshot({ path: path.join(screenshotsDir, "screenshot1.png") });
 
   const profilePageTitle = await page.textContent("h2");
   expect(profilePageTitle).toContain("Моё обучение");
 });
 
-
 test("Authorization failed", async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder("Email").fill(email2);
   await page.getByPlaceholder("Пароль").fill(password2);
+  await page.screenshot({ path: path.join(screenshotsDir, "screenshot2.png") });
   await page.getByTestId("login-submit-btn").click();
 
   const textContent = await page.textContent(
