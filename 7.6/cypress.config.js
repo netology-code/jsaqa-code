595b5f7eb8bf73cb911ffc7971011b6773cab626
gluk2309 <gluk2309@mail.ru>
2024-03-03 17:54:36 +0300

    task solved

---
+++ 7.6/cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 60000,
+  retries: 2,
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  },
+};
---
+++ 7.6/cypress/e2e/screen.js
@@ -0,0 +1,14 @@
+const desktop = {
+  width: 1920,
+  height: 1080,
+};
+
+const laptop = {
+  width: 1366,
+  height: 768,
+};
+
+module.exports = {
+  desktop,
+  laptop,
+};
