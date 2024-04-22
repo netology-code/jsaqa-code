b62733a968195fb0a0e17a0d3d0b1778f1c7ae21
Darya Vylomova <qwerty6108@gmail.com>
2024-04-22 23:48:48 +0300

    second task

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -9,6 +9,19 @@ test("validAvtorization", async ({ page }) => {
     await page.getByPlaceholder('Пароль').click();
     await page.getByPlaceholder('Пароль').fill(myPassword);
     await page.getByTestId('login-submit-btn').click();
-    await expect(page.text('Моё обучение').toBeVisible());
+    await expect(page.toHaveText('Моё обучение').toBeVisible());
+    await expect(page.toHaveURL('https://netology.ru/profile/8001073'));
+    await browser.close();
   });
+});
+test('notValidAvtorization', async ({ page }) => {
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('a@yandex.ru');
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill('123');
+  await page.getByTestId('login-submit-btn').click();
+  await expect (page.getByTestId('login-error-hint').toHaveText('Вы ввели неправильно логин или пароль').toBeVisible);
+  await browser.close();
 });
