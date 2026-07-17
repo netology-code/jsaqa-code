7cbbf9bb890619c5def55dcd611ba12e874e9c08
Svetlana Bogdan <123helga17@gmail.com>
2026-07-17 17:01:33 +0300

    Добавлены тесты авторизации на Playwright

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,60 @@
 const { test, expect } = require("@playwright/test");
+const { email, password } = require("../user");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test.setTimeout(120000);
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+test("Успешная авторизация", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in", {
+    waitUntil: "domcontentloaded",
+  });
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+  await page.getByText("Другие способы входа", { exact: true }).click();
+  await page.getByText("Войти по почте", { exact: true }).click();
 
-  page.click("text=Бизнес и управление");
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="password"]', password);
 
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+  await page
+    .getByRole("button", {
+      name: "Войти",
+      exact: true,
+    })
+    .click();
+
+  // Проверяем, что открылась страница профиля
+  await expect(page).toHaveURL(/\/profile\/\d+$/, {
+    timeout: 90000,
+  });
+
+  // Проверяем заголовок-приветствие на странице профиля
+  await expect(page.getByText(/Здравствуйте,\s*Светлана/)).toBeVisible({
+    timeout: 30000,
+  });
+});
+
+test("Неуспешная авторизация", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in", {
+    waitUntil: "domcontentloaded",
+  });
+
+  await page.getByText("Другие способы входа", { exact: true }).click();
+  await page.getByText("Войти по почте", { exact: true }).click();
+
+  // Вводим невалидные данные
+  await page.fill('input[name="email"]', "wrong@mail.ru");
+  await page.fill('input[name="password"]', "123456789");
+
+  await page
+    .getByRole("button", {
+      name: "Войти",
+      exact: true,
+    })
+    .click();
+
+  // Проверяем сообщение об ошибке
+  await expect(
+    page.getByText(/Вы ввели неправильно логин или пароль/),
+  ).toBeVisible({
+    timeout: 30000,
+  });
 });
