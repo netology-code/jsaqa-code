20377616518c7fe68c8d2448c9a29e0eb9e581aa
Svetlana Bogdan <123helga17@gmail.com>
2026-07-23 17:57:47 +0300

    Использована переменная окружения для запуска Puppeteer

--- 7.4/puppeteer/jest-puppeteer.config.js
+++ 7.4/puppeteer/jest-puppeteer.config.js
@@ -1,9 +1,13 @@
+const launch = {
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+};
+
+if (process.env.PUPPETEER_EXECUTABLE_PATH) {
+  launch.executablePath = process.env.PUPPETEER_EXECUTABLE_PATH;
+}
+
 module.exports = {
-  launch: {
-    headless: false,
-    executablePath:
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-    defaultViewport: null,
-    args: ["--start-maximized"], //— используем максимальный размер окна браузера
-  },
+  launch,
 };
