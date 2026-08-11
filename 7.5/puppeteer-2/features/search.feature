08e75fdde55ec564eb0186088a1c29d28b92473c
Svetlana Bogdan <123helga17@gmail.com>
2026-08-11 19:59:45 +0300

    Выполнено домашнее задание по Cucumber

--- 7.5/puppeteer-2/features/search.feature
+++ 7.5/puppeteer-2/features/search.feature
@@ -1,5 +1,11 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+Feature: Movie ticket booking
+
+  Scenario: Book one standard seat
+    Given user is on cinema page
+    When user books one standard seat
+    Then user sees booking confirmation
+
+Scenario: Book one vip seat
+    Given user is on cinema page
+    When user books one vip seat
+    Then user sees booking confirmation
--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -1,11 +1,27 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const {
+  Given,
+  When,
+  Then,
+  Before,
+  After,
+  setDefaultTimeout,
+} = require("cucumber");
+
+setDefaultTimeout(30000);
+
+const { clickElement, getText } = require("../../lib/commands.js");
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const browser = await puppeteer.launch({
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: false,
+    slowMo: 50,
+  });
+
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
@@ -17,18 +33,40 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+Given("user is on cinema page", async function () {
+  await this.page.goto("http://qamid.tmweb.ru/client/index.php", {
+    waitUntil: "networkidle2",
+    timeout: 0,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user books one standard seat", async function () {
+  await clickElement(
+    this.page,
+    ".movie-seances__time:not(.acceptin-button-disabled)",
+  );
+
+  await this.page.waitForSelector(".buying-scheme__chair_standart");
+
+  await clickElement(this.page, ".buying-scheme__chair_standart");
+  await clickElement(this.page, ".acceptin-button");
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("user books one vip seat", async function () {
+  await clickElement(this.page, ".page-nav__day:not(.page-nav__day_chosen)");
+
+  await clickElement(this.page, 'a[data-seance-id="243"]');
+
+  await this.page.waitForSelector(".buying-scheme__chair_vip");
+
+  await clickElement(this.page, ".buying-scheme__chair_vip");
+  await clickElement(this.page, ".acceptin-button");
+});
+
+Then("user sees booking confirmation", async function () {
+  await this.page.waitForSelector(".ticket__check-title");
+
+  const actual = await getText(this.page, ".ticket__check-title");
+
+  expect(actual).to.contain("Вы выбрали билеты");
 });
